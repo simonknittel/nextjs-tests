896fcf90a58862cc367280b303f303df48a1071e
Simon Knittel <hallo@simonknittel.de>
2023-05-26 13:11:55 +0200

    feat: set revalidate to 0

--- src/app/test1/page.tsx
+++ src/app/test1/page.tsx
@@ -4,6 +4,8 @@ import ImmediateContent from "./_components/ImmediateContent";
 import ContentSkeleton from "./_components/ContentSkeleton";
 import LoadedContentWithSkeleton from "./_components/LoadedContentWithSkeleton";
 
+export const revalidate = 0;
+
 export default function Page() {
   return (
     <main className="flex flex-col gap-8">
