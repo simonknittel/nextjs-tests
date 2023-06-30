32a3fdc24dee5660e54b67297b96ce3d9cceaac2
Simon Knittel <hallo@simonknittel.de>
2023-06-30 14:40:34 +0200

    fix: Update file location

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,14 +3,16 @@ import Link from 'next/link'
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main>
       <h1>Next.js Tests</h1>
 
       <h2>Streaming</h2>
-      <Link href="/tests/streaming/no-streaming">No streaming</Link>
-      <Link href="/tests/streaming/without-supsense">Without Suspense</Link>
-      <Link href="/tests/streaming/suspense-without-fallback">With Suspense but without fallback</Link>
-      <Link href="/tests/streaming/suspense-with-fallback">With Suspense and with fallback</Link>
+      <ul>
+        <li><Link href="/tests/streaming/no-streaming">No streaming</Link></li>
+        <li><Link href="/tests/streaming/without-supsense">Without Suspense</Link></li>
+        <li><Link href="/tests/streaming/suspense-without-fallback">With Suspense but without fallback</Link></li>
+        <li><Link href="/tests/streaming/suspense-with-fallback">With Suspense and with fallback</Link></li>
+      </ul>
     </main>
   )
 }
